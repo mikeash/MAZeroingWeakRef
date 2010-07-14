dc718a2c123efb3e850324efc9548452d029647a
mikeash <mikeash@1ea6ab17-982b-0410-b0ab-91172c90a6e5>
2010-07-14 00:20:15 +0000

    ZWR: break up main() into sections
    git-svn-id: http://www.mikeash.com/svn/ZeroingWeakRef@181 1ea6ab17-982b-0410-b0ab-91172c90a6e5

--- Source/main.m
+++ Source/main.m
@@ -27,51 +27,62 @@ int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
-    NSObject *obj = [[NSObject alloc] init];
-    NSLog(@"obj is %@", obj);
-    NSLog(@"Creating weak ref");
-    MAZeroingWeakRef *ref = [[MAZeroingWeakRef alloc] initWithTarget: obj];
-    MAZeroingWeakRef *ref2 = [[MAZeroingWeakRef alloc] initWithTarget: ref];
+    {
+        NSObject *obj = [[NSObject alloc] init];
+        NSLog(@"obj is %@", obj);
+        NSLog(@"Creating weak ref");
+        MAZeroingWeakRef *ref = [[MAZeroingWeakRef alloc] initWithTarget: obj];
+        MAZeroingWeakRef *ref2 = [[MAZeroingWeakRef alloc] initWithTarget: ref];
+        
+        [ref target];
+        NSLog(@"obj: %@  ref: %@  ref2: %@", obj, ref, ref2);
+        NSLog(@"Releasing obj");
+        [obj release];
+        NSLog(@"ref: %@  ref2: %@", ref, ref2);
+        [ref release];
+        NSLog(@"ref2: %@", ref2);
+        [ref2 release];
+    }
     
-    [ref target];
-    NSLog(@"obj: %@  ref: %@  ref2: %@", obj, ref, ref2);
-    NSLog(@"Releasing obj");
-    [obj release];
-    NSLog(@"ref: %@  ref2: %@", ref, ref2);
-    [ref release];
-    NSLog(@"ref2: %@", ref2);
-    [ref2 release];
+    {
+        NSMutableArray *array = [[NSMutableArray alloc] init];
+        NSLog(@"array: %@", array);
+        MAZeroingWeakRef *ref = [[MAZeroingWeakRef alloc] initWithTarget: array];
+        NSLog(@"array: %@  ref: %@", array, ref);
+        [array release];
+        NSLog(@"ref: %@  target: %p %@", ref, [ref target], [ref target]);
+        [ref release];
+    }
     
-    NSMutableArray *array = [[NSMutableArray alloc] init];
-    NSLog(@"array: %@", array);
-    ref = [[MAZeroingWeakRef alloc] initWithTarget: array];
-    NSLog(@"array: %@  ref: %@", array, ref);
-    [array release];
-    NSLog(@"ref: %@  target: %p %@", ref, [ref target], [ref target]);
-    [ref release];
+    {
+        NSString *str = [[NSMutableString alloc] initWithString: @"Test String"];
+        NSLog(@"str: %@", str);
+        MAZeroingWeakRef *ref = [[MAZeroingWeakRef alloc] initWithTarget: str];
+        NSLog(@"str: %@  ref: %@", str, ref);
+        [str release];
+        NSLog(@"ref: %@  target: %p %@", ref, [ref target], [ref target]);
+        [ref release];
+    }
     
-    NSString *str = [[NSMutableString alloc] initWithString: @"Test String"];
-    NSLog(@"str: %@", str);
-    ref = [[MAZeroingWeakRef alloc] initWithTarget: str];
-    NSLog(@"str: %@  ref: %@", str, ref);
-    [str release];
-    NSLog(@"ref: %@  target: %p %@", ref, [ref target], [ref target]);
-    [ref release];
+    {
+        __block BOOL cleanedUp = NO;
+        NSMutableString *str = [[NSMutableString alloc] initWithString: @"Test String"];
+        MAZeroingWeakRef *ref = [[MAZeroingWeakRef alloc] initWithTarget: str];
+        [ref setCleanupBlock: ^(id target) { cleanedUp = YES; }];
+        [str release];
+        NSLog(@"ref: %@  cleanedUp: %d", ref, cleanedUp);
+        [ref release];
+    }
     
-    __block BOOL cleanedUp = NO;
-    str = [[NSMutableString alloc] initWithString: @"Test String"];
-    ref = [[MAZeroingWeakRef alloc] initWithTarget: str];
-    [ref setCleanupBlock: ^(id target) { cleanedUp = YES; }];
-    [str release];
-    NSLog(@"ref: %@  cleanedUp: %d", ref, cleanedUp);
-    [ref release];
+    {
+        NotificationReceiver *receiver = [[NotificationReceiver alloc] init];
+        [[NSNotificationCenter defaultCenter] addWeakObserver: receiver selector: @selector(gotNote:) name: @"name" object: @"object"];
+        [[NSNotificationCenter defaultCenter] postNotificationName: @"name" object: @"object"];
+        NSLog(@"releasing receiver");
+        [receiver release];
+        [[NSNotificationCenter defaultCenter] postNotificationName: @"name" object: @"object"];
+    }
     
-    NotificationReceiver *receiver = [[NotificationReceiver alloc] init];
-    [[NSNotificationCenter defaultCenter] addWeakObserver: receiver selector: @selector(gotNote:) name: @"name" object: @"object"];
-    [[NSNotificationCenter defaultCenter] postNotificationName: @"name" object: @"object"];
-    NSLog(@"releasing receiver");
-    [receiver release];
-    [[NSNotificationCenter defaultCenter] postNotificationName: @"name" object: @"object"];
     
     NSLog(@"Done!");
     [pool drain];
