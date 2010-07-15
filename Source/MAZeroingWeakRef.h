adcc9259f7b214c2b4c66d23b1d4f4978b317574
mikeash <mikeash@1ea6ab17-982b-0410-b0ab-91172c90a6e5>
2010-07-15 20:58:20 +0000

    ZWR: add cleanup block warning and explanation of target parameter
    git-svn-id: http://www.mikeash.com/svn/ZeroingWeakRef@193 1ea6ab17-982b-0410-b0ab-91172c90a6e5

--- Source/MAZeroingWeakRef.h
+++ Source/MAZeroingWeakRef.h
@@ -24,6 +24,11 @@
 // so make it short and sweet!
 // use GCD or something to schedule execution later
 // if you need to do something that may take a while
+//
+// it is unsafe to call -target on the weak ref from
+// inside the cleanup block, which is why the target
+// is passed in as a parameter
+// note that you must not resurrect the target at this point!
 - (void)setCleanupBlock: (void (^)(id target))block;
 
 - (id)target;
